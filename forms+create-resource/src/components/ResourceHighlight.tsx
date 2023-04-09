0085d9580cc371b2c0905f61766df43a25049f38
Caio Ceretta <caioceretta@gmail.com>
2023-04-09 13:13:04 -0300

    Finished the Forms & Create Resouece topic

---
+++ forms+create-resource/src/components/ResourceHighlight.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface IResource {
+  id: number;
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+  priority: number;
+  active: boolean;
+  timeToFinish: number;
+}
+
+interface IResourcesHighlightProps {
+  resources: IResource[];
+}
+
+export default function ResourceHighlight({
+  resources,
+}: IResourcesHighlightProps) {
+  return (
+    <section className="hero ">
+      <div className="hero-body">
+        <div className="container">
+          {resources.map((resource) => (
+            <section key={resource.id} className="section">
+              <div className="columns">
+                <div className="column is-8 is-offset-2">
+                  <div className="content is-medium">
+                    <h2 className="subtitle is-4"></h2>
+                    <h1 className="title">{resource.title}</h1>
+                    <p>
+                      {resource.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
